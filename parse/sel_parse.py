5e28d45396f93ecf8d6f8d9d45e0d094ee9e40fb
Rudenches <xzdara@yandex.ru>
2025-05-21 12:32:45 +0300

    настроил селениум для навигации по сайту

--- parse/sel_parse.py
+++ parse/sel_parse.py
@@ -206,11 +206,19 @@ def start_sel(search: str):  # Запускаем парсер
         global_links = []  # Объявляем массив глобальных ссылок
         while True:  # Проходим по списку
             try:
-                link = driver.find_element(By.XPATH,
-                                           f'/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/'
-                                           f'td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[4]/div/a[1]').get_property(
-                    'href')  # Получаем ссылку
-                global_links.append(link)  # Записываем ссылку в массив глобальных ссылок
+
+                # link = driver.find_element(By.XPATH,
+                #                            f'/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/'
+                #                            f'td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[5]/div/a').get_attribute('href')  # Получаем ссылку
+                href = driver.find_element(By.XPATH, "//a[contains(@href, 'show_author_refs')]").get_attribute('href')
+
+                params = re.findall(r'show_author_refs\((\d+),\s*\'(\w+)\'\)', href)[0]
+                author_id, ref_type = params
+                url = f"https://www.elibrary.ru/author_refs.asp?id={author_id}&show_refs={ref_type}"
+                driver.get(url)
+
+
+                global_links.append(url)  # Записываем ссылку в массив глобальных ссылок
                 i += 1  # Добавляем к переменной i единицу
             except:
                 break
@@ -221,88 +229,164 @@ def start_sel(search: str):  # Запускаем парсер
             time.sleep(randint(7, 10))  # Ждём 4 секунды
             check_block(driver)  # Проверяем на блок
 
-        i = 4  # Объявляем переменную для чтения строк
+        try:
+           # select = Select(driver.find_element(By.XPATH, '//*[@id="show_hash"]'))
+                                                # '/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/'
+                                                #       'table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[2]/div[2]/table[10]/tbody/tr[2]/td/div/select'))
+
+            select_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="show_hash"]')))
+            select = Select(select_element)
+            select.select_by_value('1')
+            driver.find_element(By.XPATH,'//*[@id="show_param"]/table[12]/tbody/tr[2]/td[6]/div').click()
+                            # '/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]'
+                            # '/table/tbody/tr/td/div[2]/div[2]/table[12]/tbody/tr[2]/td[6]/div').click()
+        except Exception as e:
+            print(f"Не удалось найти или использовать select элемент: {str(e)}")
+            # Опционально, можно добавить ожидание здесь, чтобы убедиться, что страница загружена
+            time.sleep(randint(2, 5))
+            # Можно проверить состояние страницы
+            check_page_state(driver)
+
+            #time.sleep(randint(2, 5))  # Ждём 2-5 секунды
+
+        k = 4  # Объявляем переменную для чтения строк
+        reffered_arcticles = []  # массив неправильных названий статей
+        original_articles_links = [] #массиы ссылок исходных статей
         while True:  # Проходим по списку
             try:
-                if len(driver.find_elements(By.XPATH,
-                                            f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody'
-                                            f'/tr[2]/td[1] / table / tbody / tr / td / table /'
-                                            f' tbody / tr[{i}] / td[2] / span / a')) != 0:
-                    # Страница может выглядеть по-разному, поэтому проверяем на нужные данные
+                wait = WebDriverWait(driver, 10)
+                # Ждем загрузки страницы
+                time.sleep(randint(2, 4))
+                reffered_article = wait.until(
+                    EC.presence_of_element_located((
+                        By.XPATH,
+                        f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table'
+                        f'/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{k}]/td[2]/table/tbody/tr/td[1]/font/span'
+                    ))
+                ).text
+                #reffered_article = driver.find_element(By.XPATH,
+                 #                          f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table'
+                  #                         f'/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[2]/table/tbody/tr/td[1]/font/span').text
+                      # Получаем название статьи на которую ссылаются(упомянутая статья)
 
-                    line = driver.find_element(By.XPATH,
-                                               f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/'
-                                               f'table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[2]/span/a')
-                    count_quote = driver.find_element(By.XPATH,
-                                                      f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
-                                                      f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
-                                                      f'/ tr / td / table / tbody / tr[{i}] / td[3]').text  # Получаем количество цитирования
-                else:
-                    # В любом итоге получаем ссылку
-                    line = driver.find_element(By.XPATH,
-                                               f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
-                                               f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
-                                               f'/ tr / td / table / tbody / tr[{i}] / td[2] / a')
-                    count_quote = driver.find_element(By.XPATH,
-                                                      f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
-                                                      f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
-                                                      f'/ tr / td / table / tbody / tr[{i}] / td[3]').text  # Получаем количество цитирования
-
-                if count_quote == '0':
-                    # Если количество цитирования 0, то добавляем к переменной i единицу и начинаем заново
-                    i += 1
-                    continue
-
-                driver.find_element(By.XPATH,
-                                    f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
-                                    f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
-                                    f'/ tr / td / table / tbody / tr[{i}] / td[3]'
-                                    ).click()
-                x = 4
-                while True:
-                    try:
-                        if len(driver.find_elements(By.XPATH,
-                                                    f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody'
-                                                    f'/tr[2]/td[1] / table / tbody / tr / td / table /'
-                                                    f' tbody / tr[{x}] / td[2] / span / a')) != 0:
-                            line = driver.find_element(By.XPATH,
-                                               f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/'
-                                               f'table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{x}]/td[2]/span/a')
-
-                        else:
-                            line = driver.find_element(By.XPATH,
-                                               f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody'
-                                               f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
-                                               f'/ tr / td / table / tbody / tr[{x}] / td[2] / a')
-
-                        links.append(line.get_property('href'))  # Записываем ссылку в массив для ссылок
-                        x += 1  # Добавляем к переменной i единицу
-                    except:
-                        break
-
-                driver.back();
-
-                i += 1  # Добавляем к переменной i единицу
-
+                original_article = driver.find_element(By.XPATH, f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr'
+                                                                 f'/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td'
+                                                                 f'/table/tbody/tr[{k}]/td[2]/table/tbody/tr/td[1]/table[1]'
+                                                                 f'/tbody/tr/td[2]/span/a').get_property('href') #получаем ссылку на исходную статью с ошибкой
+                reffered_arcticles.append(reffered_article)  # Записываем ссылку в массив глобальных ссылок
+                original_articles_links.append(original_article)
+                k += 1  # Добавляем к переменной i единицу
             except:
-                i = 4  # Объявляем переменную для чтения строк
-                try:
-                    next = driver.find_element(By.XPATH,
-                                               "//*[contains(text(), '>>')]")  # Находим кнопку для следующей страницы
-                    if next.get_property('color') == '#aaaaaa':
-                        # Если ссылка неактивна, то заканчиваем проходить по списку
-                        break
-                    next.click()  # Нажимаем на кнопку для перехода на следующую страницу
-                    time.sleep(randint(4, 8))  # Ждём 4-8 секунд
-                    check_block(driver)  # Проверяем на блок
-                except:
-                    # Если вышла ошибка кода, то заканчиваем проходить по списку
-                    break
+                break
+
+        # for row in original_articles_links:
+        #     driver.get(row)  # Открываем ссылку из глобальных ссылок
+        #     time.sleep(randint(7, 10))  # Ждём 4 секунды
+        #     check_block(driver)  # Проверяем на блок
+
+
+        # i = 4  # Объявляем переменную для чтения строк
+        # global_links = []  # Объявляем массив глобальных ссылок
+        # while True:  # Проходим по списку
+        #     try:
+        #         link = driver.find_element(By.XPATH,
+        #                                    f'/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/'
+        #                                    f'td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[4]/div/a[1]').get_property(
+        #             'href')  # Получаем ссылку
+        #         global_links.append(link)  # Записываем ссылку в массив глобальных ссылок
+        #         i += 1  # Добавляем к переменной i единицу
+        #     except:
+        #         break
+        #
+        # links = []  # Объявляем массив для ссылок
+        # for row in global_links:
+        #     driver.get(row)  # Открываем ссылку из глобальных ссылок
+        #     time.sleep(randint(7, 10))  # Ждём 4 секунды
+        #     check_block(driver)  # Проверяем на блок
+
+        # i = 4  # Объявляем переменную для чтения строк
+        # while True:  # Проходим по списку
+        #     try:
+        #         if len(driver.find_elements(By.XPATH,
+        #                                     f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody'
+        #                                     f'/tr[2]/td[1] / table / tbody / tr / td / table /'
+        #                                     f' tbody / tr[{i}] / td[2] / span / a')) != 0:
+        #             # Страница может выглядеть по-разному, поэтому проверяем на нужные данные
+        #
+        #             line = driver.find_element(By.XPATH,
+        #                                        f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/'
+        #                                        f'table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{i}]/td[2]/span/a')
+        #             count_quote = driver.find_element(By.XPATH,
+        #                                               f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
+        #                                               f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
+        #                                               f'/ tr / td / table / tbody / tr[{i}] / td[3]').text  # Получаем количество цитирования
+        #         else:
+        #             # В любом итоге получаем ссылку
+        #             line = driver.find_element(By.XPATH,
+        #                                        f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
+        #                                        f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
+        #                                        f'/ tr / td / table / tbody / tr[{i}] / td[2] / a')
+        #             count_quote = driver.find_element(By.XPATH,
+        #                                               f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
+        #                                               f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
+        #                                               f'/ tr / td / table / tbody / tr[{i}] / td[3]').text  # Получаем количество цитирования
+        #
+        #         if count_quote == '0':
+        #             # Если количество цитирования 0, то добавляем к переменной i единицу и начинаем заново
+        #             i += 1
+        #             continue
+        #
+        #         driver.find_element(By.XPATH,
+        #                             f'/html/body/div[3]/table/tbody/tr/td/table[1]/tbody'
+        #                             f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
+        #                             f'/ tr / td / table / tbody / tr[{i}] / td[3]'
+        #                             ).click()
+                # x = 4
+                # while True:
+                #     try:
+                #         if len(driver.find_elements(By.XPATH,
+                #                                     f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody'
+                #                                     f'/tr[2]/td[1] / table / tbody / tr / td / table /'
+                #                                     f' tbody / tr[{x}] / td[2] / span / a')) != 0:
+                #             line = driver.find_element(By.XPATH,
+                #                                f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/'
+                #                                f'table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{x}]/td[2]/span/a')
+                #
+                #         else:
+                #             line = driver.find_element(By.XPATH,
+                #                                f'/html/body/div[2]/table/tbody/tr/td/table[1]/tbody'
+                #                                f'/tr/td[2]/form/table/tbody/tr[2]/td[1] / table / tbody '
+                #                                f'/ tr / td / table / tbody / tr[{x}] / td[2] / a')
+                #
+                #         links.append(line.get_property('href'))  # Записываем ссылку в массив для ссылок
+                #         x += 1  # Добавляем к переменной i единицу
+                #     except:
+                #         break
+                #
+                # driver.back();
+                #
+                # i += 1  # Добавляем к переменной i единицу
+
+            # except:
+            #     i = 4  # Объявляем переменную для чтения строк
+            #     try:
+            #         next = driver.find_element(By.XPATH,
+            #                                    "//*[contains(text(), '>>')]")  # Находим кнопку для следующей страницы
+            #         if next.get_property('color') == '#aaaaaa':
+            #             # Если ссылка неактивна, то заканчиваем проходить по списку
+            #             break
+            #         next.click()  # Нажимаем на кнопку для перехода на следующую страницу
+            #         time.sleep(randint(4, 8))  # Ждём 4-8 секунд
+            #         check_block(driver)  # Проверяем на блок
+            #     except:
+            #         # Если вышла ошибка кода, то заканчиваем проходить по списку
+            #         break
 
         result = {}  # Объявляем словарь для результата
         ix = 0  # Для упрощения объявляем порядок записи в результат
-        print(links)
-        for line in links:  # Читаем ссылки
+        print(original_articles_links)
+        for line in original_articles_links:  # Читаем ссылки
             result.update({ix: {'link': line, 'author': []}})  # Сразу подготавливаем строку в словаре
             try:
                 driver.get(line)  # Открываем страницу
